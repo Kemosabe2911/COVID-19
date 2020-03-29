5954cde5b07ae34e30b42047f6fb89b2953b14d8
Kemosabe2911 <stevinprince29@gmail.com>
2020-03-29 11:40:35 +0530

    Update header

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ window.addEventListener('scroll',function(){
     a.style.opacity= value1;
     b.style.opacity= value2;
 });
+fetchAPI();
+function fetchAPI(){
+    fetch("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Canada", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+		"x-rapidapi-key": "225859193cmshd3c0854db9f9a0ap1bdb18jsna2317bf7d964"
+	}
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data.confirmed);
+})
+.catch(err => {
+	console.log(err);
+});
+}
