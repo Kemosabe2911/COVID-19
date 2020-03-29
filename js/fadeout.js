fcaa57ae66728dc1dd7772e22bc0b6204fe4f99e
Kemosabe2911 <stevinprince29@gmail.com>
2020-03-29 15:35:36 +0530

    Update methods

--- js/fadeout.js
+++ js/fadeout.js
@@ -3,7 +3,7 @@ var b= document.getElementById('img2');
 var c= document.getElementById('img3');
 window.addEventListener('scroll',function(){
     let value1= 1+window.scrollY/-1000;
-    let value2= 1+this.window.scrollY/-2500;
+    let value2= 1+this.window.scrollY/-2900;
     a.style.opacity= value1;
     b.style.opacity= value2;
 });
